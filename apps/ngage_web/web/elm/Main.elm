25da988a3c7966d50e5f26833786f1622b4853df
daniel packard <daniel@packard.io>
2017-04-06 14:31:18 -0500

    Load events from API

--- apps/ngage_web/web/elm/Main.elm
+++ apps/ngage_web/web/elm/Main.elm
@@ -2,10 +2,27 @@ module Main exposing (..)
 
 import Components.ListView as ListView
 import Date exposing (Date)
-import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput, onCheck)
+import Dict
+import Http
+import Json.Decode as Decode
+
+
+eventDecoder =
+    Decode.map6 Event
+        (Decode.field "id" Decode.int)
+        (Decode.field "inserted_at" Decode.string)
+        (Decode.at [ "customer", "username" ] Decode.string)
+        (Decode.at [ "event_definition", "description" ] Decode.string)
+        (Decode.field "dismissed" Decode.bool)
+        (Decode.field "contacted" Decode.bool)
+
+
+eventsDecoder =
+    Decode.field "events" (Decode.list eventDecoder)
+
 
 
 -- MODEL
@@ -13,9 +30,9 @@ import Html.Events exposing (onClick, onInput, onCheck)
 
 type alias Event =
     { id : Int
-    , createdAt : Date.Date
+    , createdAt : String
     , username : String
-    , eventDefinitionId : Int
+    , eventDefinition : String
     , contacted : Bool
     , dismissed : Bool
     }
@@ -23,6 +40,7 @@ type alias Event =
 
 type alias Model =
     { loading : Bool
+    , rand : Int
     , events : List Event
     }
 
@@ -37,16 +55,12 @@ eventDefinitions =
 
 initialEvents : List Event
 initialEvents =
-    [ Event 1 (Date.fromTime 1) "pack3754@gmail.com" 1 False False
-    , Event 2 (Date.fromTime 1) "pack3754@gmail.com" 2 True True
-    , Event 3 (Date.fromTime 1) "pack3754@gmail.com" 3 False False
-    , Event 4 (Date.fromTime 1) "pack3754@gmail.com" 4 False False
-    ]
+    []
 
 
 initialModel : Model
 initialModel =
-    Model True initialEvents
+    Model True 0 []
 
 
 
@@ -54,7 +68,8 @@ initialModel =
 
 
 type Msg
-    = SetDismissed Int Bool
+    = Events (Result Http.Error String)
+    | SetDismissed Int Bool
     | ToggleContacted Int
     | SetLoadingState Bool
     | NoOp
@@ -91,11 +106,27 @@ setDismissEvent events id dismissed =
         List.map dismiss events
 
 
+parseJsonEvents : String -> List Event
+parseJsonEvents json =
+    case Decode.decodeString eventsDecoder json of
+        Ok value ->
+            value
+
+        Err err ->
+            []
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Events (Ok json) ->
+            ( { model | events = (parseJsonEvents json), loading = False }, Cmd.none )
+
+        Events (Err error) ->
+            ( model, Cmd.none )
+
         SetLoadingState loadingState ->
-            ( { model | loading = loadingState }, Cmd.none )
+            ( { model | loading = loadingState }, loadData )
 
         SetDismissed id dismissed ->
             ( { model | events = (setDismissEvent model.events id dismissed) }, Cmd.none )
@@ -107,42 +138,27 @@ update msg model =
             ( model, Cmd.none )
 
 
-getEventDescription : Int -> String
-getEventDescription eventDefinitionId =
-    case Dict.get eventDefinitionId eventDefinitions of
-        Just value ->
-            value
-
-        Nothing ->
-            "unknown event (id: " ++ (toString eventDefinitionId) ++ ")"
-
-
 eventItemView : Event -> Html Msg
 eventItemView n =
-    let
-        eventDescription =
-            getEventDescription n.eventDefinitionId
-    in
-        div [ class "event list-group-item", classList [ ( "dismissed", n.dismissed ) ], hidden False ]
-            [ span [ class "field" ] [ text (toString n.createdAt) ]
-            , span [ class "field" ] [ text n.username ]
-            , span [ class "field" ] [ text eventDescription ]
-            , label [ class "field" ]
-                [ text "contacted: "
-                , input [ class "field", type_ "checkbox", onClick (ToggleContacted n.id), checked n.contacted ] []
-                ]
-            , button [ onClick (SetDismissed n.id True), hidden (n.dismissed) ] [ text "dismiss" ]
-            , button [ class "restore-button", onClick (SetDismissed n.id False), hidden (not n.dismissed) ] [ text "restore" ]
+    div [ class "event list-group-item", classList [ ( "dismissed", n.dismissed ) ], hidden False ]
+        [ span [ class "field" ] [ text (String.slice 0 16 n.createdAt) ]
+        , span [ class "field" ] [ text n.username ]
+        , span [ class "field" ] [ text n.eventDefinition ]
+        , label [ class "field" ]
+            [ text "contacted: "
+            , input [ class "field", type_ "checkbox", onClick (ToggleContacted n.id), checked n.contacted ] []
             ]
+        , button [ onClick (SetDismissed n.id True), hidden (n.dismissed) ] [ text "dismiss" ]
+        , button [ class "restore-button", onClick (SetDismissed n.id False), hidden (not n.dismissed) ] [ text "restore" ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div [ class "content" ]
-        [ button [ onClick (SetLoadingState (not model.loading)) ] [ text "toggle loading" ]
-        , span [ class "spinner", hidden (not model.loading) ] []
+        [ span [ class "spinner", hidden (not model.loading) ] []
         , div [ class "event-feed-container", hidden model.loading ]
-            [ h3 [] [ text "Event Definitions" ]
+            [ h3 [] [ text ("Event Definitions " ++ (toString model.rand)) ]
             , ListView.view { items = Dict.toList eventDefinitions, template = Nothing }
             , h3 [] [ text "Event feed:" ]
             , ListView.view { items = model.events, template = Just eventItemView }
@@ -152,11 +168,31 @@ view model =
         ]
 
 
+
+-- COMMANDS
+
+
+loadData : Cmd Msg
+loadData =
+    Cmd.none
+
+
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( initialModel, Cmd.none )
+        { init = ( initialModel, getEvents )
         , view = view
         , update = update
         , subscriptions = (\_ -> Sub.none)
         }
+
+
+eventsUrl =
+    "http://localhost:4000/api/v1/events"
+
+
+getEvents : Cmd Msg
+getEvents =
+    eventsUrl
+        |> Http.getString
+        |> Http.send Events
